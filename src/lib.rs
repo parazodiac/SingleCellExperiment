ce7e4a36b4eef858ddc717f38690c8d03f578688
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-08-02 17:28:39 -0400

    format

--- src/lib.rs
+++ src/lib.rs
@@ -11,33 +11,33 @@ pub struct SingleCellExperiment<T> {
 
 impl<T> SingleCellExperiment<T> {
     pub fn cols(&self) -> usize {
-        return self.counts.cols()
+        return self.counts.cols();
     }
 
     pub fn rows(&self) -> usize {
-        return self.counts.rows()
+        return self.counts.rows();
     }
 
     pub fn dimensions(&self) -> (usize, usize) {
-        return (self.counts.rows(), self.counts.cols())
+        return (self.counts.rows(), self.counts.cols());
     }
 
     pub fn nnz(&self) -> usize {
-        return self.counts.nnz()
+        return self.counts.nnz();
     }
 
     pub fn row_names(&self) -> Vec<String> {
-        return self.rows.clone()
+        return self.rows.clone();
     }
 
     pub fn col_names(&self) -> Vec<String> {
-        return self.cols.clone()
+        return self.cols.clone();
     }
 
     pub fn from_csr(
-        counts: CsMat<T>, 
-        rows: Vec<String>, 
-        cols: Vec<String>
+        counts: CsMat<T>,
+        rows: Vec<String>,
+        cols: Vec<String>,
     ) -> Result<SingleCellExperiment<T>, &'static str> {
         if rows.len() != counts.rows() {
             return Err("Number of rows in the matrix doesn't match the row names");
@@ -47,7 +47,7 @@ impl<T> SingleCellExperiment<T> {
             return Err("Number of columns in the matrix doesn't match the column names");
         }
 
-        Ok(SingleCellExperiment{
+        Ok(SingleCellExperiment {
             counts: counts,
             rows: rows,
             cols: cols,
