d275845dcc1b0546615f863205a61a678dde1a57
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-11-10 14:32:43 -0500

    resolving the bug

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,8 @@ use std::path::PathBuf;
 
 pub fn read_features(file_path: PathBuf) -> Result<Vec<String>, Box<dyn Error>> {
     let file_content = std::fs::read_to_string(file_path)?;
-    let features: Vec<String> = file_content.split("\n").map(|x| x.to_owned()).collect();
+    let mut features: Vec<String> = file_content.split("\n").map(|x| x.to_owned()).collect();
+    features.pop();
 
     println!("Found {} lines", features.len());
     Ok(features)
